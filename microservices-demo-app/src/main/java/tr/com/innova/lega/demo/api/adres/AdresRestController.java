c23d444385c19e6c522ec8609525dad52f1223d1
riscue <ibrahim@ibrahimakyel.com>
2019-07-03 00:08:13 +0300

    Equals And Hashcode verifier

--- microservices-demo-app/src/main/java/tr/com/innova/lega/demo/api/adres/AdresRestController.java
+++ microservices-demo-app/src/main/java/tr/com/innova/lega/demo/api/adres/AdresRestController.java
@@ -1,6 +1,5 @@
 package tr.com.innova.lega.demo.api.adres;
 
-import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +10,6 @@ import tr.com.innova.lega.demo.shared.MappingFilter;
 import java.util.List;
 
 @RestController
-@NoArgsConstructor
 @RequestMapping("v1/adres")
 public class AdresRestController {
 
