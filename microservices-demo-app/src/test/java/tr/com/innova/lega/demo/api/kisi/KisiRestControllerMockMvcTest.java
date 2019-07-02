408707c711f227f017b5709e7de325034e4f0b3f
riscue <ibrahim@ibrahimakyel.com>
2019-07-02 20:29:38 +0300

    Test Abstraction

--- microservices-demo-app/src/test/java/tr/com/innova/lega/demo/api/kisi/KisiRestControllerMockMvcTest.java
+++ microservices-demo-app/src/test/java/tr/com/innova/lega/demo/api/kisi/KisiRestControllerMockMvcTest.java
@@ -3,7 +3,6 @@ package tr.com.innova.lega.demo.api.kisi;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mapstruct.factory.Mappers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
@@ -11,24 +10,15 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(MockitoJUnitRunner.class)
-public class KisiRestControllerMockMvcTest {
-
-    private static final String ACTUAL_ID = "1001";
-    private static final String ACTUAL_NAME = "Name";
-    private static final String ACTUAL_SURNAME = "Surname";
-
-    private KisiMapper kisiMapper = Mappers.getMapper(KisiMapper.class);
-
-    private List<Kisi> kisiList;
+public class KisiRestControllerMockMvcTest extends AbstractKisiTestSetup {
 
     @InjectMocks
     private KisiRestController mockKisiRestController;
@@ -43,22 +33,10 @@ public class KisiRestControllerMockMvcTest {
 
     @Before
     public void setup() {
-        Kisi kisi = new Kisi();
-        kisi.setId(ACTUAL_ID);
-        kisi.setAd(ACTUAL_NAME);
-        kisi.setSoyad(ACTUAL_SURNAME);
+        prepareKisi();
 
-        Kisi kisi2 = new Kisi();
-        kisi2.setId(ACTUAL_ID + "2");
-        kisi2.setAd(ACTUAL_NAME + "2");
-        kisi2.setSoyad(ACTUAL_SURNAME + "2");
-
-        kisiList = new ArrayList<>();
-        kisiList.add(kisi);
-        kisiList.add(kisi2);
-
-        when(mockKisiService.getOne(ACTUAL_ID)).thenReturn(kisi);
-        when(mockKisiMapper.mapKisiToKisiDto(kisi)).thenReturn(kisiMapper.mapKisiToKisiDto(kisi));
+        when(mockKisiService.getOne(kisiList.get(0).getId())).thenReturn(kisiList.get(0));
+        when(mockKisiMapper.mapKisiToKisiDto(kisiList.get(0))).thenReturn(kisiMapper.mapKisiToKisiDto(kisiList.get(0)));
 
         when(mockKisiService.findAll()).thenReturn(kisiList);
         when(mockKisiMapper.mapKisiListToKisiDtoList(kisiList)).thenReturn(kisiMapper.mapKisiListToKisiDtoList(kisiList));
@@ -71,24 +49,22 @@ public class KisiRestControllerMockMvcTest {
         mvc
                 .perform(get("/v1/kisi").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0]").exists())
-                .andExpect(jsonPath("$[1]").exists())
-                .andExpect(jsonPath("$[2]").doesNotExist())
-                .andExpect(jsonPath("$[0].id").value(kisiList.get(0).getId()))
-                .andExpect(jsonPath("$[0].ad").value(kisiList.get(0).getAd()))
-                .andExpect(jsonPath("$[0].soyad").value(kisiList.get(0).getSoyad()))
-                .andExpect(jsonPath("$[1].id").value(kisiList.get(1).getId()))
-                .andExpect(jsonPath("$[1].ad").value(kisiList.get(1).getAd()))
-                .andExpect(jsonPath("$[1].soyad").value(kisiList.get(1).getSoyad()));
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].id", is(kisiList.get(0).getId())))
+                .andExpect(jsonPath("$[0].ad", is(kisiList.get(0).getAd())))
+                .andExpect(jsonPath("$[0].soyad", is(kisiList.get(0).getSoyad())))
+                .andExpect(jsonPath("$[1].id", is(kisiList.get(1).getId())))
+                .andExpect(jsonPath("$[1].ad", is(kisiList.get(1).getAd())))
+                .andExpect(jsonPath("$[1].soyad", is(kisiList.get(1).getSoyad())));
     }
 
     @Test
     public void findById() throws Exception {
         mvc
-                .perform(get("/v1/kisi/" + ACTUAL_ID).contentType(MediaType.APPLICATION_JSON))
+                .perform(get("/v1/kisi/" + kisiList.get(0).getId()).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").doesNotExist())
-                .andExpect(jsonPath("$.ad").value(ACTUAL_NAME))
-                .andExpect(jsonPath("$.soyad").value(ACTUAL_SURNAME));
+                .andExpect(jsonPath("$.ad").value(kisiList.get(0).getAd()))
+                .andExpect(jsonPath("$.soyad").value(kisiList.get(0).getSoyad()));
     }
 }
--- microservices-demo-app/src/test/java/tr/com/innova/lega/demo/api/kisi/KisiRestControllerUnitTest.java
+++ microservices-demo-app/src/test/java/tr/com/innova/lega/demo/api/kisi/KisiRestControllerUnitTest.java
@@ -3,12 +3,10 @@ package tr.com.innova.lega.demo.api.kisi;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mapstruct.factory.Mappers;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import tr.com.innova.lega.demo.shared.MappingFilter;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -16,43 +14,22 @@ import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
-public class KisiRestControllerUnitTest {
-
-    private static final String ACTUAL_ID = "1001";
-    private static final String ACTUAL_NAME = "Name";
-    private static final String ACTUAL_SURNAME = "Surname";
+public class KisiRestControllerUnitTest extends AbstractKisiTestSetup {
 
     @Mock
     private KisiRepository kisiRepository;
-
-    private List<Kisi> kisiList;
-
-    private KisiMapper kisiMapper = Mappers.getMapper(KisiMapper.class);
 
     private KisiRestController kisiRestController;
 
     @Before
     public void before() {
         prepareKisi();
-        when(kisiRepository.getOne(ACTUAL_ID)).thenReturn(kisiList.get(0));
+
+        when(kisiRepository.getOne(kisiList.get(0).getId())).thenReturn(kisiList.get(0));
+
         when(kisiRepository.findAll()).thenReturn(kisiList);
+
         kisiRestController = new KisiRestController(new KisiServiceImpl(kisiRepository), kisiMapper);
-    }
-
-    private void prepareKisi() {
-        Kisi kisi = new Kisi();
-        kisi.setId(ACTUAL_ID);
-        kisi.setAd(ACTUAL_NAME);
-        kisi.setSoyad(ACTUAL_SURNAME);
-
-        Kisi kisi2 = new Kisi();
-        kisi2.setId(ACTUAL_ID + "2");
-        kisi2.setAd(ACTUAL_NAME + "2");
-        kisi2.setSoyad(ACTUAL_SURNAME + "2");
-
-        kisiList = new ArrayList<>();
-        kisiList.add(kisi);
-        kisiList.add(kisi2);
     }
 
     @Test
@@ -69,8 +46,8 @@ public class KisiRestControllerUnitTest {
 
     @Test
     public void findById() {
-        MappingFilter response = kisiRestController.findById(ACTUAL_ID);
-        verify(kisiRepository, times(1)).getOne(ACTUAL_ID);
+        MappingFilter response = kisiRestController.findById(kisiList.get(0).getId());
+        verify(kisiRepository, times(1)).getOne(kisiList.get(0).getId());
 
         assertNotNull(response.getValue());
         assertEquals(kisiMapper.mapKisiToKisiDto(kisiList.get(0)), response.getValue());
